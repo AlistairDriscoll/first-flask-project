2f8f5ecc4902a7843ca1407b15f4ea2964529a72
AlistairDriscoll <pallythecoder@gmail.com>
2024-06-01 12:05:11 +0100

    Add comment

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
 
+# test
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
